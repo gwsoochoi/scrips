fa412dbd691ca5831d7e3b0bf6232869ea7e680f
Gwangsoo Choi <gwsoochoi@gmail.com>
2023-03-04 01:43:19 +0900

    Changed rename pattern

--- rename_images.sh
+++ rename_images.sh
@@ -3,15 +3,18 @@
 echo "Start file rename"
 
 image_extensions=".jpg .jpeg .png .gif .psd"
+count=0
+enc="$(openssl rand -hex 10)"
 
 for file in *; do
 	if [[ -f "$file" ]]; then
 		extension="${file##*.}"
-	
+		
 		if [[ $image_extensions =~ $extension ]]; then
-			newname="$(openssl rand -hex 10).$extension"
+			newname="$enc$count.$extension"
 			echo "Renaming $file to $newname"
 			mv "$file" "$newname"
+			count=$((count+1))
 		fi
 	fi
 done
